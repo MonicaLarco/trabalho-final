dd3651bfed542b3e5bf6764159f80d5fcdc3b1b3
mlarco <mlarco94@gmail.com>
2022-07-24 17:59:55 -0300

    Third version

--- app/routes/apiRouter.js
+++ app/routes/apiRouter.js
@@ -1,19 +1,84 @@
 
-const express = require ('express');
-let apiRouter = express.Router();
+require('dotenv').config();
+const express = require('express');
+const apiRouter = express.Router();
+const knex = require('knex') ({
+    client: 'pg',
+    connection: {
+        connectionString : process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    }
+});
 
-// const endpoint = '/';
-const lista_produtos = {
-    produtos: [
-        { id: 1, descricao: "Produto 1", valor: 5.00, marca: "marca "  },
-        { id: 2, descricao: "Produto 2", valor: 5.00, marca: "marca "  },
-        { id: 3, descricao: "Produto 3", valor: 5.00, marca: "marca "  },
-    ]
-}
+apiRouter.get('/produtos', (req, res) => {
+    knex.select('*').from('produto')
+    .then(produtos => res.status(200).json(produtos));  
+});
 
-apiRouter.get('/produtos', function (req, res) {
-    res.status(200).json(lista_produtos.produtos);
-})
+// Add a new product
+apiRouter.post('/produtos', express.json(), (req, res) => {
+    knex('produto')
+        .insert({
+            descricao: req.body.descricao,
+            valor: req.body.valor,
+            marca: req.body.marca},
+            ['id', 'descricao', 'valor', 'marca'])
+        .then (produtos => {
+            let produto = produtos[0]
+            res.status(201).json ({ produto })
+        })
+        .catch (err => res.status(500).json ({ message: `An error has occured ${err.message}`}))
+});
 
+// Get a product by ID
+apiRouter.get('/produtos/:id', (req, res) => {
+    let id = Number.parseInt(req.params.id);
+    if (id > 0) {
+        knex
+          .select("*")
+          .from("produto")
+          .where('id', id)
+          .then(produtos => res.json(produtos))
+          .catch(err => { res.status(500).json({message: 'Could not fetch - ' + err.message})});
+    } else {
+        res.status(404).json({ message: "Product not found!" });
+    }
+});
 
-module.exports = apiRouter;
+// Update Product details
+apiRouter.put('/produtos/:id', (req, res) => {
+    let id = Number.parseInt(req.params.id);
+    if (id > 0) {
+        knex('produto')
+            .where('id', id)
+            .update({
+                descricao: req.body.descricao,
+                valor: req.body.valor,
+                marca: req.body.marca
+            },
+            ['id','descricao','valor','marca'])
+            .then (produtos => {
+                let produto = produtos[0]
+                res.status(200).json({produto})
+            })
+            .catch (err => res.status(500).json ({ message: `An error has occured ${err.message}`}));
+    } else {
+        res.status(404).json({message: "Product not found!"});
+    }
+});
+
+// Delete a Product
+apiRouter.delete('/produtos/:id', (req, res) => {
+    let id = Number.parseInt(req.params.id);
+    if (id > 0) {
+        knex('produto')
+          .where('id', id)
+          .del()
+          .then(res.status(200).json({message: `Item ${id} was removed from the list successfully`}))
+          .catch (err => res.status(500).json ({ message: `An error has occured ${err.message}`}))
+    } else {
+        res.status(404).json({ message: "Product not found"});
+    }
+});
+
+module.exports = apiRouter
--- app/server.js
+++ app/server.js
@@ -1,20 +1,18 @@
-require('dotenv').config();
-
+require('dotenv').config({path: "./app/.env"});
 const express = require ('express');
 const cors = require('cors');
 const path  = require ('path');
 const app = express();
+const morgan = require('morgan')
+const apiRouter = require('./routes/apiRouter');
+const port = process.env.PORT || 3000;
 
+app.use (morgan ('common'))
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/app', express.static (path.join (__dirname, '/public')));
-
-const apiRouter = require('./routes/apiRouter');
+app.use('/app', express.static(path.join (__dirname, '/public')));
 app.use('/api', apiRouter);
 
-
-let port = process.env.PORT || 3000;
-app.listen (port, () => console.info(`Listening on ${ port }`));
-
+app.listen(port, () => console.info(`Listening on ${ port }`));
