9b5418335c4f3927af1731e195ad9462306e181c
mlarco <mlarco94@gmail.com>
2022-08-14 14:50:27 -0300

    Added login feature

---
+++ app/public/index.js
@@ -0,0 +1,15 @@
+// const button = document.querySelector('#submit-form');
+// const loginInput = document.querySelector('#loginInput');
+// const passwordInput = document.querySelector('#passwordInput');
+
+// button.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const xhr = new XMLHttpRequest();
+//   if (xhr.readyState == 4) {
+//     if (xhr.status == 200) {
+
+//     }
+
+//   }
+
+// });
